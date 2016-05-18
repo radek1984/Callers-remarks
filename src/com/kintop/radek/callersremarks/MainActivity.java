9342e84f258e8f455eeda4a2ab9279f16000b3b9
Radek Kintop <radek.kintop@gmail.com>
2016-05-19 00:06:25 +0200

    -Call button handling -Take photo button handling

--- src/com/kintop/radek/callersremarks/MainActivity.java
+++ src/com/kintop/radek/callersremarks/MainActivity.java
@@ -2,14 +2,20 @@ package com.kintop.radek.callersremarks;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,8 +45,34 @@ public class MainActivity extends Activity {
 		}
 		else
 		{
-			Toast.makeText(this, "Could not find ant connections", Toast.LENGTH_LONG).show();
+			Toast.makeText(this, "Could not find any connections", Toast.LENGTH_LONG).show();
 		}
+	}
+
+	public void onCallButtonClick(View bttn)
+	{
+		String tel_nuum = (String)bttn.getTag();
+		Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tel_nuum.trim()));
+        startActivity(callIntent);
+	}
+	public void onTxtNoteButtonClick(View bttn)
+	{
+		
+	}
+	public void onPhotoNoteButtonClick(View bttn)
+	{
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	     //       Uri.withAppendedPath(mLocationForPhotos, targetFilename));
+
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(takePictureIntent, 0);
+	    }
+
+	}
+	public void onVoiceNoteButtonClick(View bttn)
+	{
+		
 	}
 }
 
@@ -64,7 +96,9 @@ class CallsListAdapter<T> extends  BaseAdapter
 			vi = inflater.inflate(R.layout.list_item, null);
 		TextView textview = (TextView) vi.findViewById(R.id.textView_number);
 		textview.setText((String)values[position]);
-
+		ImageButton button = (ImageButton)vi.findViewById(R.id.imageButton_call);
+		button.setTag(new String((String)values[position]));
+		
 		return vi;
 	}
 
